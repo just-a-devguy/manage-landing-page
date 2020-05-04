516db4f896390aa5aacae381f2dd3a7986213a60
Adler Luders <adlerluders@gmail.com>
2020-05-04 14:26:30 -0400

    completed email validation

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,1 +1,29 @@
-console.log("connected");
+// UI Elements
+const UI = {
+  errMssg: "#err-mssg",
+  emailBtn: "#btn",
+  email: "#email-input",
+};
+
+const emailSubmit = document.querySelector(UI.emailBtn),
+  invalidMssg = document.querySelector(UI.errMssg),
+  email = document.querySelector(UI.email);
+
+// Functionalities
+function emailValidation(e) {
+  e.preventDefault();
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,7})$/;
+
+  if (!re.test(email.value)) {
+    invalidMssg.style.display = "block";
+    email.style.border = "1px solid red";
+    email.style.color = "red";
+  } else {
+    invalidMssg.style.display = "none";
+    email.style.border = "none";
+    email.style.color = "initial";
+  }
+}
+
+// Event Listeners
+emailSubmit.addEventListener("click", emailValidation);
