0c7737a24e70130ae154bc58a0491d4180504d55
Adler Luders <adlerluders@gmail.com>
2020-05-11 08:44:25 -0400

    finished responsive carousel

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,13 +5,19 @@ const UI = {
   email: "#email-input",
   testimonial: ".testimonials__item",
   togglers: ".testimonials__toggler",
+  hamburger: ".mobile-nav__burger",
+  navToggler: ".mobile-nav__toggler",
+  navItems: ".main-nav__items",
 };
 
 const emailSubmit = document.querySelector(UI.emailBtn),
   invalidMssg = document.querySelector(UI.errMssg),
+  email = document.querySelector(UI.email),
   testimonial = document.querySelectorAll(UI.testimonial),
   tabTogglers = document.querySelectorAll(UI.togglers),
-  email = document.querySelector(UI.email);
+  hamburger = document.querySelector(UI.hamburger),
+  navToggler = document.querySelector(UI.navToggler),
+  navItems = document.querySelector(UI.navItems);
 
 // Functionalities
 function emailValidation(e) {
@@ -46,8 +52,22 @@ function displayItem() {
   this.style.listStyle = "disc";
 }
 
+// Mobile Nav
+// console.log(hamburger);
+// console.log(navItems);
+function showMenu() {
+  navItems.style.display = "flex";
+  // this.children[0].innerHTML = "<img src='../../images/icon-close.svg' />";
+  // console.log(this.children.currentSrc);
+  navToggler.innerHTML = "<img src='images/icon-close.svg' />";
+  console.log(navToggler);
+}
+
 // Event Listeners
 emailSubmit.addEventListener("click", emailValidation);
+
+hamburger.addEventListener("click", showMenu);
+
 togglerArr.forEach((toggler) => {
   toggler.addEventListener("click", displayItem);
 });
